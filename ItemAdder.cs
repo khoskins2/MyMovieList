def417c738755b78180d37a9f864252b6dda870e
Dustin Bailey <dbailey22@my.apsu.edu>
2023-04-03 16:29:43 -0500

    made Item adder class shell

---
+++ ItemAdder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for ItemAdder
+/// </summary>
+public class ItemAdder
+{
+	public ItemAdder()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
